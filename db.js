582f61d160afc46f36be5ea25971b88c355e6f94
gestionece <63755239+gestionece@users.noreply.github.com>
2021-01-13 19:15:01 +0100

    Create db.js

---
+++ db.js
@@ -0,0 +1,126 @@
+let nevCalcTable = {
+    EUP: [
+
+
+        [
+            {
+                "key" : "8400149083",
+                "label": "Parma",
+            },
+            {
+                "filter": "MF",
+                "value": 4.15
+            },
+            {
+                "filter": "TF",
+                "value": 6.00
+            },
+            {
+                "filter": "M2",
+                "value": 6.00
+            }
+        ],
+        [
+            {
+                "key" : "8400150707",
+                "label": "Ferrara",
+            },
+            {
+                "filter": "MF",
+                "value": 4.15
+            },
+            {
+                "filter": "TF",
+                "value": 6.00
+            },
+            {
+                "filter": "M2",
+                "value": 6.00
+            }
+        ],
+
+    ],
+    CEP: [
+        {
+            "label": "Eseguiti MF-TF",
+            "filter": "MF",
+            "key": "CON",
+            "value": 2.50
+        }, {
+            "label": "Acesso a Vuoto MF-TF",
+            "filter": "MF",
+            "key": "AV",
+            "value": 1.20
+        }, {
+            "label": "Eseguiti TF15/30",
+            "filter": "TF",
+            "key": "CON",
+            "value": 3.5
+        }, {
+            "label": "Acesso a Vuoto TF15/30",
+            "filter": "TF",
+            "key": "AV",
+            "value": 1.20
+        }, {
+            "label": "Eseguiti M2",
+            "filter": "M2",
+            "key": "CON",
+            "value": 4.5
+        }, {
+            "label": "Acesso a Vuoto M2",
+            "filter": "M2",
+            "key": "AV",
+            "value": 1.20
+        }, {
+            "label": "Recuperi Eseguiti MF-TF",
+            "filter": "MF-R",
+            "key": "CON",
+            "value": 3.5
+        }, {
+            "label": "Recuperi Acesso a Vuoto MF-TF",
+            "filter": "MF-R",
+            "key": "AV",
+            "value": 1.20
+        }, {
+            "label": "Recuperi Eseguiti TF15/30",
+            "filter": "TF-R",
+            "key": "CON",
+            "value": 3.5
+        }, {
+            "label": "Recuperi Acesso a Vuoto TF15/30",
+            "filter": "TF-R",
+            "key": "AV",
+            "value": 1.20
+        }, { //GG MF-R
+            "label": "Eseguiti <30 Giorni MF-TF",
+            "filter": "MF-R",
+            "key": "GG1",
+            "value": 3.50
+        }, {
+            "label": "Eseguiti 30<>90 Giorni MF-TF",
+            "filter": "MF-R",
+            "key": "GG2",
+            "value": 2.50
+        }, {
+            "label": "Eseguiti 90<>120 Giorni MF-TF",
+            "filter": "MF-R",
+            "key": "GG3",
+            "value": 1.50
+        }, {
+            "label": "Eseguiti <30 Giorni  TF15/30",
+            "filter": "TF-R",
+            "key": "GG1",
+            "value": 3.50
+        }, {
+            "label": "Eseguiti 30<>90 Giorni TF15/30",
+            "filter": "TF-R",
+            "key": "GG2",
+            "value": 2.50
+        }, {
+            "label": "Eseguiti 90<>120 Giorni TF15/30",
+            "filter": "TF-R",
+            "key": "GG3",
+            "value": 1.50
+        }
+    ]
+}
